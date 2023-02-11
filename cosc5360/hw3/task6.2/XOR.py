e64be128703a9717cc2e50202f6eab5078978168
Edward F. Ward III <4785890+edwardfward@users.noreply.github.com>
2023-02-11 14:27:05 -0600

    testing latex equation

---
+++ cosc5360/hw3/task6.2/XOR.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Program from Computer and Internet Security 3rd Edition by Wenliang Du
+# Chapter 24.5.2 pg. 578
+
+from sys import argv
+
+_, first, second = argv
+aa = bytearray.fromhex(first)
+bb = bytearray.fromhex(second)
+xord = bytearray(x^y for x,y in zip(aa,bb))
+print(xord.hex())
